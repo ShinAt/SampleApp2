c9753f54e78c1db9440ccc2f59e0e094ca1b4fb3
ShinAt55 <sinntarou836271@gmail.com>
2019-04-28 16:29:20 +0900

    アカウント有効化機能追加

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'rails-i18n'
 gem 'faker',         '1.9.3'
 gem 'will_paginate', '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
+gem 'mail-iso-2022-jp', '2.0.8'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,20 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       #ユーザーログイン後にユーザー情報のページにリダイレクト
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        #エラーメッセージの作成
+        message = "アカウントは有効化されていません。"
+        message += "送信したメールの有効化リンクをクリックしてください。"
+        flash.now[:warning] = message
+        redirect_to root_url
+      end
     else
-      #エラーメッセージの作成
-      flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています'
+      flash.now[:danger] = "メールアドレスまたはパスワードが間違っています。"
       render 'new'
-      
     end
   end
 
